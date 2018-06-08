acda8a06bdc8ee0e4193b1467765084858927ae9
LACKNC <LACKNC@rpi.edu>
2018-06-08 16:24:46 -0400

    Added inital Action Adapter Frame for Missing Data Simulator

---
+++ Source/Adapters/MissingData/MissingDataSim.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using GSF.TimeSeries;
+using GSF.TimeSeries.Adapters;
+
+namespace MissingData
+{
+    public class MissingDataSim : ActionAdapterBase
+    {
+        public override bool SupportsTemporalProcessing => throw new NotImplementedException();
+
+        protected override void PublishFrame(IFrame frame, int index)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
